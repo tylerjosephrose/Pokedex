996e4bbccd9ca93b8103a00b9dec157c6ed88d96
Tyler Rose <tylerjosephrose@gmail.com>
2017-01-25 09:19:09 -0500

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Tyler Rose on 1/25/17.
+//  Copyright © 2017 Tyler Rose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
